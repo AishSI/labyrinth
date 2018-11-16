d42f8a6d0c4a6967f5c79d6591e04562842af3ec
AishSI <aishsi@gmail.com>
2018-11-17 04:06:10 +0700

    доделал змейку

--- SnakeMazeTask.cs
+++ SnakeMazeTask.cs
@@ -6,8 +6,12 @@ namespace Mazes
 	{
 		public static void MoveOut(Robot robot, int width, int height)
         {
-            SnakeToRight(robot, width);
-            SnakeToLeft(robot, width);
+            while (robot.Y < height - 2)
+            {
+                OneSegmentSnake(robot, width);
+                if (robot.Y < height - 2)
+                    MoveToNextSegment(robot, width);
+            }
         }
 
         public static void MoveTolWall(Robot robot, int count, Direction direction)
@@ -16,22 +20,16 @@ namespace Mazes
                 robot.MoveTo(direction);
         }
 
-        public static void SnakeToRight(Robot robot, int width)
+        public static void OneSegmentSnake(Robot robot, int width)
         {
             MoveTolWall(robot, width, Direction.Right);
             MoveTolWall(robot, 5, Direction.Down);
-        }
-
-        public static void SnakeToLeft(Robot robot, int width)
-        {
             MoveTolWall(robot, width, Direction.Left);
-            MoveTolWall(robot, 5, Direction.Down);
         }
 
-        //public static void MoveToHorizontalWall(Robot robot, int count, Direction direction)
-        //{
-        //    while (robot.Y < count - 2)
-        //        robot.MoveTo(direction);
-        //}
+        public static void MoveToNextSegment(Robot robot, int width)
+        {            
+            MoveTolWall(robot, 5, Direction.Down);
+        } 
     }
 }
