25710254bceaa8924a1e7f673cc38a553a8f85df
AishSI <aishsi@gmail.com>
2018-11-17 03:54:06 +0700

    в пустом лабиринте обошелся одним методомю начал делать "змейку"

--- EmptyMazeTask.cs
+++ EmptyMazeTask.cs
@@ -6,20 +6,14 @@ namespace Mazes
     {
         public static void MoveOut(Robot robot, int width, int height)
         {
-            MoveToVerticalWalll(robot, width, Direction.Right);
-            MoveToHorizontalWall(robot, height, Direction.Down);
+            MoveToWall(robot, width, Direction.Right);
+            MoveToWall(robot, height, Direction.Down);
         }
 
-        public static void MoveToVerticalWalll(Robot robot, int count, Direction direction)
+        public static void MoveToWall(Robot robot, int count, Direction direction)
         {
-            while (robot.X < count - 2)
+            for (int i = 0; i < count - 3; i++)
                 robot.MoveTo(direction);
-        }
-
-        public static void MoveToHorizontalWall(Robot robot, int count, Direction direction)
-        {
-            while (robot.Y < count - 2)
-                robot.MoveTo(direction);
-        }
+        } 
     }
 }
--- SnakeMazeTask.cs
+++ SnakeMazeTask.cs
@@ -1,9 +1,37 @@
-﻿namespace Mazes
+﻿using System;
+
+namespace Mazes
 {
 	public static class SnakeMazeTask
 	{
 		public static void MoveOut(Robot robot, int width, int height)
-		{
-		}
-	}
+        {
+            SnakeToRight(robot, width);
+            SnakeToLeft(robot, width);
+        }
+
+        public static void MoveTolWall(Robot robot, int count, Direction direction)
+        {
+            for (int i = 0; i < count - 3; i++ )            
+                robot.MoveTo(direction);
+        }
+
+        public static void SnakeToRight(Robot robot, int width)
+        {
+            MoveTolWall(robot, width, Direction.Right);
+            MoveTolWall(robot, 5, Direction.Down);
+        }
+
+        public static void SnakeToLeft(Robot robot, int width)
+        {
+            MoveTolWall(robot, width, Direction.Left);
+            MoveTolWall(robot, 5, Direction.Down);
+        }
+
+        //public static void MoveToHorizontalWall(Robot robot, int count, Direction direction)
+        //{
+        //    while (robot.Y < count - 2)
+        //        robot.MoveTo(direction);
+        //}
+    }
 }
