21712acaa0b3c4d69dc983e27c38a6aaed09dc8b
AishSI <aishsi@gmail.com>
2018-11-16 22:32:19 +0700

    Add project files.

---
+++ EmptyMazeTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mazes
+{
+    public static class EmptyMazeTask
+    {
+        public static void MoveOut(Robot robot, int width, int height)
+        {
+            int longSide = Math.Max(width, height);
+            for (int x = 0; x < longSide - 3; x++)
+            {
+                if (robot.X < width - 2) robot.MoveTo(Direction.Right);
+                if (robot.Y < height - 2) robot.MoveTo(Direction.Down);
+            }
+        }
+    }  
+}
