b0a4629300eb03cf844dcb91710d7a88487b5a20
AishSI <aishsi@gmail.com>
2018-11-16 22:48:25 +0700

    вынес шаги с проверками (не уперся ли в стену) в отдельные методы.

--- EmptyMazeTask.cs
+++ EmptyMazeTask.cs
@@ -9,9 +9,19 @@ namespace Mazes
             int longSide = Math.Max(width, height);
             for (int x = 0; x < longSide - 3; x++)
             {
-                if (robot.X < width - 2) robot.MoveTo(Direction.Right);
-                if (robot.Y < height - 2) robot.MoveTo(Direction.Down);
+                NoVerticallWall(robot, width);
+                NoHorisontalWall(robot, height);
             }
         }
-    }  
+
+        public static void NoVerticallWall(Robot robot, int width)
+        {
+            if (robot.X < width - 2) robot.MoveTo(Direction.Right);
+        }
+
+        public static void NoHorisontalWall(Robot robot, int height)
+        {
+            if (robot.Y < height - 2) robot.MoveTo(Direction.Down);
+        }
+    }
 }
